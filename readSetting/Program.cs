30ccd0a0c5c513dcb1558f1a03f6f8ec88a6349b
cosmictinydust <cosmictinydust@hotmail.com>
2019-07-04 10:36:06 +0800

    从appsetting.json中读取值,把日志输出到控制台

--- readSetting/Program.cs
+++ readSetting/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Threading.Tasks;
@@ -11,24 +12,25 @@ namespace readSetting
     {
         static async Task Main(string[] args)
         {
-            var builder=new HostBuilder()
+            var builder = new HostBuilder()
+                .ConfigureLogging((hostContext, config) =>
+                {
+                    config.AddConsole();
+                    //config.AddDebug();
+                })
                 .ConfigureAppConfiguration((hostContext, config) =>
                 {
-                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+                    config.AddCommandLine(args);
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddOptions();
+                    services.AddLogging();
                     services.Configure<vaulesInSetting>(hostContext.Configuration.GetSection("mySettings"));
-                })
-                .Build();
-            using (builder)
-            {
-                await builder.StartAsync();
-                var test = builder.Services.GetService<IOptions<vaulesInSetting>>();
-                Console.WriteLine($"Get values .Value1:{test.Value.Value1}");
-                Console.ReadKey();
-            }
+                    services.AddHostedService<TimerHostedService>();
+                });
+            await builder.RunConsoleAsync();
         }
     }
 }
---
+++ readSetting/TimerHostedService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace readSetting
+{
+    class TimerHostedService:BackgroundService
+    {
+        private readonly vaulesInSetting _settings;
+        private ILogger _logger;
+        private Timer _timer;
+
+        public TimerHostedService(IOptionsSnapshot<vaulesInSetting> settings, ILogger<TimerHostedService> logger)
+        {
+            _settings = settings.Value;
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogWarning($"From appsettings.json value1:{_settings.Value1}");
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation($"Program is running . {DateTime.Now},value2:{_settings.Value2}");
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timer is stopping!");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            _timer?.Dispose();
+            base.Dispose();
+        }
+    }
+}
